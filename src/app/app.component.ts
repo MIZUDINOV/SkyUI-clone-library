b6460281fda03d1e58321c0838c1121cbe068603
Mukhamed <izudinov.work@gmail.com>
2022-01-02 19:22:33 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'sky-ui';
-}
+export class AppComponent {}
