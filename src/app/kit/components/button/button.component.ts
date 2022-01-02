e3b4e6e693ed0d2cfc43f3e964438667cb36d3ad
Mukhamed <izudinov.work@gmail.com>
2022-01-02 19:35:55 +0300

    #001: button component init

---
+++ src/app/kit/components/button/button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 's-button',
+    templateUrl: './button.template.html',
+    styleUrls: ['./button.style.scss'],
+})
+export class SuiButtonComponent {}
