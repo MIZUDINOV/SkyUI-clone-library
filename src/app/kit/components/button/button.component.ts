08f7c1a427c03c00447765aa052899e51e4b08a3
Mukhamed <izudinov.work@gmail.com>
2022-01-03 10:51:40 +0300

    #001: add ChangeDetectionStrategy.onPush

--- src/app/kit/components/button/button.component.ts
+++ src/app/kit/components/button/button.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
     selector: 's-button',
     templateUrl: './button.template.html',
     styleUrls: ['./button.style.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SuiButtonComponent {}
