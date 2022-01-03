fe8059d3f486e8345da0343c5b33b64f9649d9c9
Mukhamed <izudinov.work@gmail.com>
2022-01-03 17:25:16 +0300

    #002: default prop decorator

--- src/cdk/decorators/default-prop.ts
+++ src/cdk/decorators/default-prop.ts
@@ -1,1 +1,10 @@
-export function suiDefaultProp() {}
+import { SuiBooleanHandler } from '../types/handler';
+
+export function suiDefaultProp<T extends object, K extends keyof T>(
+    assertion?: SuiBooleanHandler<T[K]>,
+    ...args: any[]
+): PropertyDecorator {
+    return (target, key) => {
+        const { name } = target.constructor;
+    };
+}
